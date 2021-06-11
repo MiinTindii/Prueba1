b0cf33e8859874778cc814c3a6dc927d425da247
MiinTindii <lea.collao@duocuc.cl>
2021-06-11 19:21:37 -0400

    Trabajando Clima

--- clima.js
+++ clima.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
-    console.log("JQuery esta activo ");
-
     function showPosition(position) {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
+        console.log(lat);
+        console.log(lon);
+        var urlLugar = `https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`
+        console.log(urlLugar);
 
-            console.log(lat);
-            console.log(lon);
+    
+        $.get(urlLugar,function(Lugares){
+            console.log(Lugares[0]);
+            
 
+        });
+        
 
-    }
-
-
-
-});
+      }
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function(posision){
+            showPosition(posision);
+        })
+  
+          }else{
+            alert("navergador no soporta la geolocalizacion")
+          }
+  
+    })
